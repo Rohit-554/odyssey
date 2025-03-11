e3700744a5517c952c5d71e92776a863048b77ae
Jadu <48874687+Rohit-554@users.noreply.github.com>
2025-03-12 00:12:18 +0530

    update const

---
+++ lib/core/utils/ImageConst.dart
@@ -0,0 +1,13 @@
+class ImageConst{
+  static const appLogo = "assets/images/lg_logos/lg-ai-fictional-groq.png";
+  static const gsoc = "assets/images/lg_logos/line_2/Gsoc.png";
+  static const lg = "assets/images/lg_logos/line_2/Lg.png";
+  static const flutter = "assets/images/lg_logos/line_3/flutter_lleida.png";
+  static const lgEU = "assets/images/lg_logos/line_3/LogoLGEU.png";
+  static const lgLab = "assets/images/lg_logos/line_3/lg_lab.png";
+  static const gdg = "assets/images/lg_logos/line_3/flutter_lleida.png";
+  static const parcLleida = "assets/images/lg_logos/line_3/Parc_.png";
+  static const splashOnline = 'https://raw.githubusercontent.com/Rohit-554/LaserSlidesFlutter/master/lgAIGroqSplash.png';
+  static const splashOnline2 = 'https://raw.githubusercontent.com/Rohit-554/LaserSlidesFlutter/master/Lg%20logos%20(634%20x%20531%20px).png';
+  static const splashOnline3 = 'https://raw.githubusercontent.com/Rohit-554/odyssey/refs/heads/master/lg_logo.png';
+}
